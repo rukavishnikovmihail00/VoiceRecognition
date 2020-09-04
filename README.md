67ed8efa5ec72666ed9b27dde647e3578ab99581
Mikhail Rukavishnikov <61332555+rukavishnikovmihail00@users.noreply.github.com>
2020-09-04 16:44:12 +0300

    GUI added

--- README.md
+++
@@ -1,4 +0,0 @@
-# VoiceRecognition
-The project using speech recognition python module
-
-There is no serious code yet, but it`s gonna be
--- main.py
+++ main.py
@@ -2,9 +2,14 @@ import speech_recognition as sr
 from bs4 import BeautifulSoup as BS
 import requests
 import pyttsx3
+from tkinter import *
 
-speak_engine = pyttsx3.init()
 
+def speak(phrase):
+    speak_engine = pyttsx3.init()
+    speak_engine.say(phrase)
+    speak_engine.runAndWait()
+    speak_engine.stop()
 
 def getWeather():
     url = requests.get("https://sinoptik.ua/погода-нижний-новгород")
@@ -13,27 +18,33 @@ def getWeather():
     for el in soup.find('div', {'class': 'description'}):
         arr.append(el)
     phrase = arr[2]
-    speak_engine.say(phrase)
-    speak_engine.runAndWait()
-    speak_engine.stop()
+    speak(phrase)
+
+def start():
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        print("Я слушаю")
+        audio = r.listen(source)
+
+    try:
+        voice = r.recognize_google(audio, language="ru-RU").lower()
+        print(voice)
+
+        if voice == "погода":
+            getWeather()
+        else:
+            speak("Я не поняла вас")
+
+    except sr.UnknownValueError:
+        print("Recognition error")
+    except sr.RequestError as e:
+        print("Connection error {0}".format(e))
 
 
-r = sr.Recognizer()
-with sr.Microphone() as source:
-    print("Я слушаю")
-    audio = r.listen(source)
-
-try:
-    voice = r.recognize_google(audio, language="ru-RU").lower()
-    print(voice)
-
-    if voice == "погода":
-        getWeather()
-
-except sr.UnknownValueError:
-    print("Recognition error")
-except sr.RequestError as e:
-    print("Unknown error; {0}".format(e))
-
-
+root = Tk()
+root.title("Voice Assistant 1.0")
+root.geometry("300x250")
+btn = Button(text="Start", command=start)
+btn.pack()
+root.mainloop()
 
